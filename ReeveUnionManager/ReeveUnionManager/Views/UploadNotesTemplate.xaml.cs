244cbdfd7a3fa2f4fb67002a49ec2eb6958dcafb
calebwisneski-ops <caleb.wisneski@gmail.com>
2025-11-10 19:55:39 -0600

    Allowed for basic entries to be able to upload and capture photos, updated AndroidManifest and Info.plist with needed permissions

--- ReeveUnionManager/ReeveUnionManager/Views/UploadNotesTemplate.xaml.cs
+++ ReeveUnionManager/ReeveUnionManager/Views/UploadNotesTemplate.xaml.cs
@@ -1,9 +1,18 @@
 namespace ReeveUnionManager.Views;
+
+using ReeveUnionManager.Models;
+using Microsoft.Maui.Media;
+using Microsoft.Maui.Storage;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
 /// <summary>
 /// Author: Caleb Wisneski
 /// </summary>
 public partial class UploadNotesTemplate : ContentView
 {
+    FileResult photo;
+
     public static readonly BindableProperty TitleProperty
         = BindableProperty.Create(nameof(Title), typeof(string), typeof(UploadNotesTemplate), string.Empty);
 
@@ -16,5 +25,31 @@ public partial class UploadNotesTemplate : ContentView
     public UploadNotesTemplate()
     {
         InitializeComponent();
+        BindingContext = MauiProgram.businessLogic;
     }
+
+    public async void OnUploadPhotoClicked(object sender, EventArgs args)
+    {
+        FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+    }
+
+    public async void OnCapturePhotoClicked(object sender, EventArgs args)
+    {
+        FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+    }
+    /* 
+    public async void HandleSubmit(object sender, EventArgs args)
+    {
+
+        string notes = NotesBox.Text;
+
+
+        CallLogError error = await MauiProgram.businessLogic.AddCallLog(callId, callerName, timeOfCall, callNotes);
+        if (error != CallLogError.None)
+        {
+            await DisplayAlert("Addition has failed", error.ToString(), "OK");
+        }
+    }
+
+    */
 }
