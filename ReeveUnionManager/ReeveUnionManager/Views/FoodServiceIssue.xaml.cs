e7787c51a0ebdd6598d67b2154967212cf7718ce
calebwisneski-ops <caleb.wisneski@gmail.com>
2025-11-24 17:11:47 -0600

    Updated UI of Food Service to better match that of other pages

--- ReeveUnionManager/ReeveUnionManager/Views/FoodServiceIssue.xaml.cs
+++ ReeveUnionManager/ReeveUnionManager/Views/FoodServiceIssue.xaml.cs
@@ -1,11 +1,86 @@
 namespace ReeveUnionManager.Views;
+
+using System.Collections.ObjectModel;
+using ReeveUnionManager.Models;
+using Microsoft.Maui.Media;
+using Microsoft.Maui.Storage;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
 /// <summary>
 /// <!-- Kevin Kraiss -->
 /// </summary>
 public partial class FoodServiceIssue : ContentPage
 {
+
+    public ObservableCollection<PhotoInfo> Photos { get; set; } = new();
+    FileResult photo;
+
     public FoodServiceIssue()
     {
         InitializeComponent();
+        BindingContext = this;
+    }
+
+    public async void OnUploadPhotoClicked(object sender, EventArgs args)
+    {
+        FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+        var newFile = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+        using (var stream = await photo.OpenReadAsync())
+        using (var newStream = File.OpenWrite(newFile))
+        {
+            await stream.CopyToAsync(newStream);
+        }
+            
+        if (photo != null)
+        {
+            Photos.Add(new PhotoInfo{
+                Image = ImageSource.FromFile(newFile), FileName = photo.FileName});
+        }
+    }
+
+
+    public async void OnCapturePhotoClicked(object sender, EventArgs args)
+    {
+        FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+        var newFile = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+        using (var stream = await photo.OpenReadAsync())
+        using (var newStream = File.OpenWrite(newFile))
+        {
+            await stream.CopyToAsync(newStream);
+        }
+
+        if (photo != null)
+        {
+            Photos.Add(new PhotoInfo
+            {
+                Image = ImageSource.FromFile(newFile),
+                FileName = photo.FileName
+            });
+        }
+    }
+    
+    public async void RemoveImage(object sender, EventArgs args)
+    {
+        
+        if (sender is Button button && button.CommandParameter is PhotoInfo photo)
+        {
+
+            Photos.Remove(photo);
+
+        }
+    }
+
+    public async void HandleSubmit(object sender, EventArgs args)
+    {
+        /*
+        string notes = NotesBox.Text;
+
+        BasicEntryError error = await MauiProgram.businessLogic.AddBasicEntry(Title, notes, Photos);
+        if (error != BasicEntryError.None)
+        {
+            //await DisplayAlert("Addition has failed", error.ToString(), "OK");
+        }
+        */
     }
 }
